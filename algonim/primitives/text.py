6a3bb57ac2fd0e32f9e2e5b1694df58a35302bd7
Saegl <saegl@protonmail.com>
2026-01-11 23:48:31 +0500

    Add missing methods on `Text`

--- algonim/primitives/text.py
+++ algonim/primitives/text.py
@@ -5,12 +5,20 @@ from algonim.colors import Color
 
 class Text:
     def __init__(self, x, y, text: str, font_size: int = 32):
+        self.x = x
+        self.y = y
         self.label = pyglet.text.Label(
             text, x, y, anchor_x="center", anchor_y="center", font_size=font_size
         )
 
     def set_color(self, color: Color):
         self.label.color = color
+
+    def set_x(self, x):
+        self.label.x = x
+
+    def set_y(self, y):
+        self.label.y = y
 
     def draw(self):
         self.label.draw()
