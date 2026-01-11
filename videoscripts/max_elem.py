6f799f8394ce2dc85a6670bb07ad0223ce5a140c
Saegl <saegl@protonmail.com>
2026-01-12 00:26:32 +0500

    Add fades demo

--- videoscripts/max_elem.py
+++ videoscripts/max_elem.py
@@ -1,5 +1,12 @@
 from algonim.window import AppWindow
-from algonim.script import Script, animate_creation, fade_in, wait, move_down
+from algonim.script import (
+    Script,
+    fade_in,
+    fade_out,
+    move_up,
+    wait,
+    move_down,
+)
 from algonim.primitives.array import Array
 from algonim.primitives.text import Text
 
@@ -17,13 +24,16 @@ def build_script(window: AppWindow):
 
     script.do(fade_in(text))
     script.do(move_down(text, 100, 1))
-    script.do(wait(1.0))
+    script.do(wait(0.5))
 
     arr_data = [4, 3, 1, 7, 20, 7, 5]
     arr = Array(win_center_x, win_center_y + 100, arr_data)
     window.objects.append(arr)
 
-    script.do(animate_creation(arr))
-    script.do(animate_creation(arr))
+    script.do(fade_in(arr))
+    script.do(fade_out(arr))
+
+    script.do(move_up(text, 100, 1.0))
+    script.do(fade_out(text))
 
     return script
